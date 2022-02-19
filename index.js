74e8c32c539113dcef854a7ed9ddc85df35ce69b
lucas melo <lucasmellosilva97@hotmail.com>
2022-02-19 14:00:19 -0300

    add conexao com socket.io

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-let app = require('express')();
-let http = require('http').createServer(app);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const port = 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/pages/index.html');
 });
 
-http.listen(port, mensage);
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => console.log('user disconnected'))
+})
 
-function mensage(){
-  console.log('server running in port 3000')
-}
+http.listen(port, () => console.log('server is running on port 3000'));
